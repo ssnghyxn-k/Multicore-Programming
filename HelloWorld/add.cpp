5ff7befe56e485503dc56f64b8b4eb97e7c33ec4
ssnghyxn-k <kimsunghyun8312@gmail.com>
2025-09-21 17:05:01 +0900

    thread 생성

--- HelloWorld/add.cpp
+++ HelloWorld/add.cpp
@@ -4,9 +4,19 @@
 using namespace std;
 
 int sum;
+
+void worker()
+{
+	for(auto i = 0; i < 50000000; ++i) 
+		sum = sum + 2;
+}
+
 int main()
 {
-	for (auto i = 0; i < 50000000; ++i) 
-		sum = sum + 2;
+	thread t1(worker);
+	thread t2(worker);
+	t1.join();
+	t2.join();
+
 	cout << "Sum = " << sum << endl;
 }
