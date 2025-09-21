0cb35b2ff03c22bc1001505662151f1b495e1ccc
ssnghyxn-k <kimsunghyun8312@gmail.com>
2025-09-21 15:38:19 +0900

    1억 만들기

--- HelloWorld/add.cpp
+++ HelloWorld/add.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <thread>
 
+using namespace std;
+
+int sum;
+int main()
+{
+	for (auto i = 0; i < 50000000; ++i) 
+		sum = sum + 2;
+	cout << "Sum = " << sum << endl;
+}
