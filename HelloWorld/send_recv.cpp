9bf0e06001a43701d01a2203a554f16ea1a287c1
ssnghyxn-k <kimsunghyun8312@gmail.com>
2025-09-22 19:11:39 +0900

    mutex 추가 -> 데드락 발생

--- HelloWorld/send_recv.cpp
+++ HelloWorld/send_recv.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
 
 int g_data = 0;
 bool g_ready = false;
+std::mutex sr_lock;		// send-receive lock
 
 void recv()
 {
+	sr_lock.lock();
 	while (false == g_ready);
 	std::cout << "Data = " << g_data << std::endl;
+	sr_lock.unlock();
 }
 
 void send()
 {
+	sr_lock.lock();
 	std::cin >> g_data;
 	g_ready = true;
+	sr_lock.unlock();
 }
 
 int main()
